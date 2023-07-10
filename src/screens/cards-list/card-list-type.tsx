828c78008a055e4ef3a4f7e7da65a264d52c1a7e
bibin-jaimon <bibinjaimon@gmail.com>
2023-07-11 02:14:08 +0530

    Add Transaction Type

--- src/screens/cards-list/card-list-type.tsx
+++ src/screens/cards-list/card-list-type.tsx
@@ -1,11 +1,17 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackType } from '../../navigation/root-stack-type';
 
+export interface Transaction {
+  note: string;
+  amount: string;
+  currency: string;
+  date: string;
+}
 export interface Card {
   id: number;
   name: string;
-  bgColor: string;
   bgImage: string;
+  transactions: Transaction[];
 }
 
 export interface CardViewPropType {
